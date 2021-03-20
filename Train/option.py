18339e081f280f51bdc4923fcf540534a347af8e
JeeKing <1406011307@qq.com>
2021-03-20 11:24:55 +0800

    Update option.py

--- Train/option.py
+++ Train/option.py
@@ -1,7 +1,7 @@
 import argparse
 import template
 
-parser = argparse.ArgumentParser(description='EDSR and MSRN, MDRN')
+parser = argparse.ArgumentParser(description='EDSR and TSAN')
 
 parser.add_argument('--debug', action='store_true',
                     help='Enables debug mode')
@@ -19,7 +19,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed')
 
 # Data specifications
-parser.add_argument('--dir_data', type=str, default='/mnt/data/div2k_v1/',
+parser.add_argument('--dir_data', type=str, default='/mnt/data/div2k_v1/',  #input your training data path
                     help='dataset directory')
 parser.add_argument('--dir_demo', type=str, default='test',
                     help='demo image directory')
